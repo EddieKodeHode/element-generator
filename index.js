186be71e9fbab8fecbca8e4a73d34b609d500a95
EddieKodeHode <eddie.kodehode@gmail.com>
2021-10-14 10:05:58 +0200

    more js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ let elementHere = document.getElementById("element-here")
 let descHere = document.getElementById("desc-here")
 // determines path for description here
 
+let rarity = 0
+// sets starting rarity
+
+let commonElement = 0
+// sets starting element
+
+let rareElement = 0
+// sets starting element
+
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
