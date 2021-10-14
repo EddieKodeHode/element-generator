56389f9efff8f4e55bb0320a5475d627785c1289
EddieKodeHode <eddie.kodehode@gmail.com>
2021-10-14 09:53:40 +0200

    added a bunch of stuff

---
+++ index.js
@@ -0,0 +1,15 @@
+let elementHere = document.getElementById("element-here")
+// determines path for element here.
+
+let descHere = document.getElementById("desc-here")
+// determines path for description here
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// randomNumber(1, 20)
+// Random number generator. change min and max. returns whole numbers.
+
+
+
+// Common are water, fire, wind, earth, and healing. Rare elements are nature, ice, poison, lightening, shadow, and light.
